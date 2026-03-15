fd85070c490ae64732f5d4b453d19178ce0669f6
BenjaUrbin <benjamin.urbina@userena.cl>
2026-03-15 18:59:05 -0300

    Add auth endpoints for frontend integration

--- appMTG/urls.py
+++ appMTG/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from . import views
-from .views import register, InventarioCartasViewSet, saveCardOne
+from .views import current_user, register, InventarioCartasViewSet, saveCardOne
 
 router = DefaultRouter()
 router.register(r'mis-inventarios', views.MisInventariosViewSet, basename='mis-inventarios')
@@ -11,6 +12,9 @@ inventario_cartas_list = InventarioCartasViewSet.as_view({"get": "list"})
 urlpatterns = [
     path('', include(router.urls)),
     path('auth/register/', register, name='register'),
+    path('auth/login/', TokenObtainPairView.as_view(), name='login'),
+    path('auth/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+    path('auth/me/', current_user, name='current-user'),
 
     # Ejemplo: http://localhost:8000/api/usuarios/2/inventarios/Prueba/cartas/
     path(
